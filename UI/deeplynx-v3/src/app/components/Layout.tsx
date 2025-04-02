a2d77988b521e9f62102d86cc00d0ba8830acaea
Daniel Uribe <daniel.uribeporfirio@inl.gov>
2025-04-02 12:56:22 -0600

    Initial commit. Created Layout/Banner. Added DaisyUI

--- UI/deeplynx-v3/src/app/components/Layout.tsx
+++ UI/deeplynx-v3/src/app/components/Layout.tsx
@@ -2,43 +2,15 @@ import React from "react";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-base-100">
-      {/* Navbar */}
-      <header className="navbar bg-primary text-white px-6 shadow-lg">
-        {/* Left Side - Logo */}
-        <div className="flex-1">
-          <a className="btn btn-ghost text-2xl font-bold">
-            <img src="/logo.svg" alt="DeepLynx" className="h-8 mr-2" />
-            DeepLynx
-          </a>
-        </div>
-
-        {/* Middle - Search Bar */}
-        <div className="flex-none">
-          <div className="form-control">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-96"
-            />
-          </div>
-        </div>
-
-        {/* Right Side - User Icon */}
-        <div className="flex-none">
-          <button className="btn btn-circle btn-outline">
-            <span className="material-icons">account_circle</span>
-          </button>
-        </div>
+    <div className="flex flex-col min-h-screen">
+      {/* Banner/Header */}
+      <header className="bg-blue-900 text-white flex justify-between items-center px-6 py-4 shadow-md">
+        <div className="text-xl font-bold">DeepLynx</div>
+        <div className="text-sm">User</div>
       </header>
 
       {/* Page Content */}
-      <main className="flex-grow p-6">{children}</main>
-
-      {/* Floating Add Button */}
-      <button className="btn btn-circle btn-primary fixed bottom-6 right-6 shadow-lg">
-        <span className="text-2xl">+</span>
-      </button>
+      <main className="flex-grow bg-gray-200 p-6">{children}</main>
     </div>
   );
 };
--- UI/deeplynx-v3/src/app/page.tsx
+++ UI/deeplynx-v3/src/app/page.tsx
@@ -5,8 +5,8 @@ import Layout from "./components/Layout";
 
 function Home({ Component, pageProps }: AppProps) {
   return (
-    <div className="p-4 bg-primary text-black">
-      If this text is styled, Tailwind & DaisyUI are working!
+    <div className="p-4 text-black">
+      DeepLynx HomePage!
     </div>
   );
 }
