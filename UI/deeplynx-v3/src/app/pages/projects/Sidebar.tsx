a7b32e57bf6c99eb2d1a8f7bcb3d62de754f53cd
Daniel Uribe <daniel.uribeporfirio@inl.gov>
2025-04-14 13:23:43 -0600

    Added Create Projects modal and added toggle to change themes.

--- UI/deeplynx-v3/src/app/pages/projects/Sidebar.tsx
+++ UI/deeplynx-v3/src/app/pages/projects/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -6,7 +6,6 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
-  // ---------- Sidebar toggel effects ----------
   const sidebarRef = useRef<HTMLDivElement>(null);
   const [buttonPosition, setButtonPosition] = useState(0);
 
@@ -16,40 +15,39 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
       setButtonPosition(isOpen ? sidebarWidth : 0);
     }
   }, [isOpen]);
-  // ---------- Sidebar toggel effects ends ----------
 
   return (
-    <div className="relative bg-base-400">
+    <div className="relative">
       <div
         ref={sidebarRef}
-        className={`fixed top-16 left-0 h-[calc(100%-64px)] bg-base-200 shadow-lg transform ${
+        className={`fixed top-16 left-0 h-[calc(100%-64px)] bg-neutral-content shadow-lg transform ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300 ease-in-out z-40`}
         style={{ width: isOpen ? "30%" : "0" }}
       >
         {isOpen && (
-          <div className='p-10'>
+          <div className="p-10">
             <div className="card-body p-4">
-              <h2 className="card-title">Welcome to DeepLynx v3!</h2>
-              <p className="pt-5">
+              <h2 className="card-title text-neutral">Welcome to DeepLynx v3!</h2>
+              <p className="pt-5 text-neutral">
                 DeepLynx is a unique datawarehouse designed to provide easy
                 collaboration on large projects. DeepLynx allows users to define
-                an ontology and then store data under it. Find more information on
-                our wiki by clicking here.
+                an ontology and then store data under it. Find more information
+                on our wiki by clicking here.
               </p>
             </div>
-            <h2 className='card-title pb-4'>FAQ's</h2>
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <h2 className="card-title pb-4 text-neutral">FAQ's</h2>
+            <div className="collapse collapse-plus bg-neutral-content border border-neutral">
               <input type="radio" name="my-accordion-3" defaultChecked />
               <div className="collapse-title font-semibold">
                 How do I create an account?
               </div>
               <div className="collapse-content text-sm">
-                Click the "Sign Up" button in the top right corner and follow the
-                registration process.
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
               </div>
             </div>
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <div className="collapse collapse-plus bg-neutral-content border border-neutral">
               <input type="radio" name="my-accordion-3" />
               <div className="collapse-title font-semibold">
                 I forgot my password. What should I do?
@@ -59,7 +57,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
                 instructions sent to your email.
               </div>
             </div>
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <div className="collapse collapse-plus bg-neutral-content border border-neutral">
               <input type="radio" name="my-accordion-3" />
               <div className="collapse-title font-semibold">
                 How do I update my profile information?
@@ -74,7 +72,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
       </div>
 
       <button
-        className="fixed top-20 bg-blue-900 text-white px-4 py-2 rounded-r-full shadow-md transition-transform duration-300 ease-in-out z-50"
+        className="fixed top-20 bg-primary text-white px-4 py-2 rounded-r-full shadow-md transition-transform duration-300 ease-in-out z-50"
         style={{ left: `${buttonPosition}px` }}
         onClick={toggleSidebar}
       >
