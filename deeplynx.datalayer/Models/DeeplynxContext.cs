0bfee39683f10fe25151eb5e0786ed9805329fd8
Lilianne Cantillo <Lilianne.Cantillo@inl.gov>
2025-04-22 14:38:10 -0400

    Deleted and not needed code

--- deeplynx.datalayer/Models/DeeplynxContext.cs
+++ deeplynx.datalayer/Models/DeeplynxContext.cs
@@ -36,10 +36,6 @@ public partial class DeeplynxContext : DbContext
     public virtual DbSet<Role> Roles { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Class>(entity =>
