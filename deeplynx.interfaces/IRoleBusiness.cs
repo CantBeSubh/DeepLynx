dd973570d67074d2d2614d65c8443a71dae85fe5
dylan-kuncheria <Dylan.Kuncheria@inl.gov>
2025-05-06 14:56:57 -0500

    Role: Creation of deeplynx.interfaces.IRoleBusiness.cs file

---
+++ deeplynx.interfaces/IRoleBusiness.cs
@@ -0,0 +1,13 @@
+using deeplynx.models;
+
+namespace deeplynx.interfaces
+{
+    public interface IRoleBusiness
+    {
+        IEnumerable<Role> GetAllRoles(long projectId);
+        Role GetRole(long projectId, long roleId);
+        Role CreateNewRole(Role role);
+        Role UpdateRole(long projectId, long roleId, Role role);
+        bool DeleteRole(long projectId, long roleId);
+    }
+}
