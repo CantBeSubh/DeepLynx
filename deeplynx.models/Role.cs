a2f2208830bc40b7e61df348a7d28bba5793fd06
dylan-kuncheria <Dylan.Kuncheria@inl.gov>
2025-05-06 14:42:40 -0500

    Role: Created deeplynx.models.Role.cs file

---
+++ deeplynx.models/Role.cs
@@ -0,0 +1,9 @@
+namespace deeplynx.Models
+{
+    public class Role
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long ProjectId { get; set; }
+    }
+}
