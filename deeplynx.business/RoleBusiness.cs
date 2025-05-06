4aa5eebdd1121ffdf8428b9a1e76bd7252c3ec04
dylan-kuncheria <Dylan.Kuncheria@inl.gov>
2025-05-06 15:47:47 -0500

    Role: Created deeplynx.business.RoleBusiness.cs file

---
+++ deeplynx.business/RoleBusiness.cs
@@ -0,0 +1,85 @@
+using deeplynx.datalayer.Models;
+using deeplynx.interfaces;
+using deeplynx.models;
+
+namespace deeplynx.business
+{
+    public class RoleBusiness : IRoleBusiness
+    {
+        private readonly DeepLynxContext _context;
+
+        public RoleBusiness(DeepLynxContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Role> GetAllRoles(long projectId)
+        {
+            return _context.Roles.Where(r => r.ProjectId == projectId && r.DeletedAt == null).Select(r => new Role
+            {
+                Id = r.Id,
+                Name = r.Name,
+                ProjectId = r.ProjectId
+            }).ToList();
+        }
+
+        public Role GetRole(long projectId, long roleId)
+        {
+            var role = _context.Roles.FirstOrDefault(r => r.Id == roleId && r.ProjectId == projectId && r.DeletedAt == null);
+
+            if (role == null)
+                throw new Exception("Role not found");
+
+            return new Role
+            {
+                Id = role.Id,
+                Name = role.Name,
+                ProjectId = role.ProjectId
+            };
+        }
+
+        public Role CreateNewRole(Role role)
+        {
+            if (role == null)
+                throw new ArgumentNullException(nameof(role));
+
+            var newRole = new datalayer.Models.Role
+            {
+                Name = role.Name,
+                ProjectId = role.ProjectId
+            };
+
+            _context.Roles.Add(newRole);
+            _context.SaveChanges();
+
+            role.Id = newRole.Id;
+            return role;
+        }
+
+        public Role UpdateRole(long projectId, long roleId, Role role)
+        {
+            var existing = _context.Roles.FirstOrDefault(r => r.Id == roleId && r.ProjectId == projectId && r.DeletedAt == null);
+
+            if (existing == null)
+                throw new Exception("Role not found");
+
+            existing.Name = role.Name
+
+            _context.SaveChanges();
+
+            return role;
+        }
+
+        public bool DeleteRole(long projectId, long roleId)
+        {
+            var existing = _context.Roles.FirstOrDefault(r => r.Id == roleId && r.ProjectId == projectId && r.DeletedAt == null);
+            if (existing == null)
+                return false;
+
+            existing.DeletedAt = DateTime.UtcNow; 
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
