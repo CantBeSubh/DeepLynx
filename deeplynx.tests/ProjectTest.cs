aa559365778d901f9c59ba211a0c756e9736ed85
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-05-12 16:23:16 -0600

    Testcontainers working

--- deeplynx.tests/ProjectTest.cs
+++ deeplynx.tests/ProjectTest.cs
@@ -1,42 +1,47 @@
-using Testcontainers.PostgreSql;
-using deeplynx.business; 
+using deeplynx.business;
 using deeplynx.datalayer.Models;
-using deeplynx.models;
+using Testcontainers.PostgreSql;
 using Microsoft.EntityFrameworkCore;
-
+using deeplynx.models;
 
 namespace deeplynx.tests;
 
 public sealed class ProjectTest : IAsyncLifetime
 {
-    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
-        .WithImage("postgres:15-alpine")
-        .Build();
+    private DeeplynxContext _context;
 
-    public Task InitializeAsync()
-    {   
-        return _postgres.StartAsync();
+    private readonly PostgreSqlContainer _postgresContainer = new PostgreSqlBuilder()
+         .WithImage("postgres:15-alpine")
+         .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _postgresContainer.StartAsync();
+
+        var options = new DbContextOptionsBuilder<DeeplynxContext>()
+            .UseNpgsql(_postgresContainer.GetConnectionString())
+            .Options;
+
+        _context = new DeeplynxContext(options);
+        await _context.Database.MigrateAsync(); 
     }
 
-    public Task DisposeAsync()
+    public async Task DisposeAsync()
     {
-        return _postgres.DisposeAsync().AsTask();
+        await _postgresContainer.DisposeAsync();
     }
 
     [Fact]
     async public void ShouldReturn3Projects()
     {
-      
-        var projectBusiness = new ProjectBusiness(new DeeplynxContext(new DbContextOptionsBuilder<DeeplynxContext>()
-            .UseNpgsql(_postgres.GetConnectionString())
-            .Options));
-        
+
+        var projectBusiness = new ProjectBusiness(_context);
+
         await projectBusiness.CreateProject(new ProjectRequestDto { Name = "Project One", Abbreviation = "P1" });
         await projectBusiness.CreateProject(new ProjectRequestDto { Name = "Project Two", Abbreviation = "P2" });
-        await projectBusiness.CreateProject(new ProjectRequestDto { Name = "Project Three"});
+        await projectBusiness.CreateProject(new ProjectRequestDto { Name = "Project Three" });
         var project = await projectBusiness.GetAllProjects();
 
-        // Then
         Assert.Equal(3, project.Count());
     }
 }
