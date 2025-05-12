37aeb1d9ac43cd24c8230a8585ee69c8ffc7a6ff
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-05-12 16:40:00 -0600

    Reverse back to original context

--- deeplynx.datalayer/Models/DeeplynxContext.cs
+++ deeplynx.datalayer/Models/DeeplynxContext.cs
@@ -44,7 +44,10 @@ public partial class DeeplynxContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     public virtual DbSet<UserProject> UserProjects { get; set; }
-    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Name=ConnectionStrings:DefaultConnection");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Class>(entity =>
