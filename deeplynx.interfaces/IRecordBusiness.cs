439bd366623af99e5c1f0bf098a0bca25162a6d8
Lilianne Cantillo <Lilianne.Cantillo@inl.gov>
2025-05-15 11:08:18 -0400

    Added Soft delete on Records
    Added  RecordRequestDto

--- deeplynx.interfaces/IRecordBusiness.cs
+++ deeplynx.interfaces/IRecordBusiness.cs
@@ -6,10 +6,10 @@ namespace deeplynx.interfaces;
 
 public interface IRecordBusiness
 {
-    Task<IEnumerable<Record>> GetAllRecords(long projectId, long dataSourceId);
-    Task<Record> GetRecord(long projectId, long dataSourceId, long recordId);
-    Task<Record> CreateRecord(long projectId, long dataSourceId, RecordRequestDto dto);
-    Task<Record> UpdateRecord(long projectId, long dataSourceId, long recordId, RecordRequestDto dto);
+    Task<IEnumerable<RecordResponseDto>> GetAllRecords(long projectId, long dataSourceId);
+    Task<RecordResponseDto> GetRecord(long projectId, long dataSourceId, long recordId);
+    Task<RecordResponseDto> CreateRecord(long projectId, long dataSourceId, RecordRequestDto dto);
+    Task<RecordResponseDto> UpdateRecord(long projectId, long dataSourceId, long recordId, RecordRequestDto dto);
     Task<bool> DeleteRecord(long projectId, long dataSourceId, long recordId);
     int CalculateJsonMaxDepth(JsonNode node); 
 }
