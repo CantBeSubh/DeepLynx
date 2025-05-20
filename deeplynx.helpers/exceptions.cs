927f3bcff2db42e25e08f92fb24e3d8c76409141
Ed R <145260780+era-zx@users.noreply.github.com>
2025-05-19 18:10:25 -0600

    added support for custom exceptions

---
+++ deeplynx.helpers/exceptions.cs
@@ -0,0 +1,6 @@
+namespace deeplynx.helpers.exceptions;
+
+public class ProjectDependencyDeletionException : Exception
+{
+    public ProjectDependencyDeletionException(string message) : base(message) { }
+}
