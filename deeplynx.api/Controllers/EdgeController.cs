f06631fc492072dd9acebc9eb3c042661314d967
Jaren Brownlee <jaren.brownlee@inl.gov>
2025-05-20 15:43:38 -0500

    add comments and response dto for edges

--- deeplynx.api/Controllers/EdgeController.cs
+++ deeplynx.api/Controllers/EdgeController.cs
@@ -7,15 +7,26 @@ namespace deeplynx.api.Controllers
 {
     [ApiController]
     [Route("api/projects/{projectId}/edges")]
+    
     public class EdgeController : ControllerBase
     {
         private readonly IEdgeBusiness _edgeBusiness;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EdgeController"/> class
+        /// </summary>
+        /// <param name="edgeBusiness">The business logic interface for handling edge operations.</param>
         public EdgeController(IEdgeBusiness edgeBusiness)
         {
             _edgeBusiness = edgeBusiness;
         }
 
+        /// <summary>
+        /// Retrieves all edges for a specific project and (optionally) datasource
+        /// </summary>
+        /// <param name="projectId">The ID of the project whose edges are to be retrieved</param>
+        /// <param name="dataSourceId">(Optional) The ID of the datasource by which to filter edges</param>
+        /// <returns>A list of edges based on the applied filters.</returns>
         [HttpGet("GetAllEdges")]
         public async Task<IActionResult> GetAllEdges(long projectId, [FromQuery] long? dataSourceId = null)
         {
@@ -32,24 +43,40 @@ namespace deeplynx.api.Controllers
             }
         }
 
+        /// <summary>
+        /// Retrieves a specific edge by its origin and destination IDs
+        /// OR Retrieves an edge by its id
+        /// </summary>
+        /// <param name="edgeId">The id whereby to fetch the edge</param>
+        /// <param name="originId">the origin ID by which to fetch the edge if no ID</param>
+        /// <param name="destinationId">the destination ID by which to fetch the edge if no ID</param>
+        /// <returns>The edge associated with the given id or origin/destination combo</returns>
         [HttpGet("GetEdge")]
         public async Task<IActionResult> GetEdge(
-            [FromQuery, Required] long originId, 
-            [FromQuery, Required] long destinationId)
+            [FromQuery] long edgeId,
+            [FromQuery] long originId, 
+            [FromQuery] long destinationId)
         {
             try
             {
-                var edge = await _edgeBusiness.GetEdge(originId, destinationId);
+                var edge = await _edgeBusiness.GetEdge(edgeId, originId, destinationId);
                 return Ok(edge);
             }
             catch (Exception exc)
             {
-                var message = $"An error occurred while retrieving edge with origin {originId} and destination {destinationId}: {exc}";
+                var message = $"An error occurred while retrieving edge: {exc}";
                 NLog.LogManager.GetCurrentClassLogger().Error(message);
                 return StatusCode(StatusCodes.Status500InternalServerError, message);
             }
         }
 
+        /// <summary>
+        /// Retrieves a specific edge by its origin and destination IDs
+        /// Asynchronously creates a new edge for a specified project.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to which the edge belongs</param>
+        /// <param name="dataSourceId">The ID of the data source to which the edge belongs</param>
+        /// <param name="edge">The edge request data transfer object containing edge details</param>
         [HttpPost("CreateEdge")]
         public async Task<IActionResult> CreateEdge(long projectId, [Required] long dataSourceId, [FromBody] EdgeRequestDto edge)
         {
@@ -66,38 +93,62 @@ namespace deeplynx.api.Controllers
             }
         }
 
+        /// <summary>
+        /// Updates an existing edge by its ID or origin/destination.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to which the edge belongs.</param>
+        /// <param name="edge">The edge request data transfer object containing updated edge details.</param>
+        /// <param name="edgeId">The ID of the edge to update</param>
+        /// <param name="originId">The origin ID of the edge to update if edgeID is not present.</param>
+        /// <param name="destinationId">The destination ID of the edge if edgeID is not present.</param>
+        /// <returns>The updated edge response DTO with its details</returns>
+        /// <returns></returns>
         [HttpPut("UpdateEdge")]
         public async Task<IActionResult> UpdateEdge(
-            [FromQuery, Required] long originId, 
-            [FromQuery, Required] long destinationId, 
+            long projectId,
+            [FromQuery] long edgeId,
+            [FromQuery] long originId, 
+            [FromQuery] long destinationId, 
             [FromBody] EdgeRequestDto edge)
         {
             try
             {
-                var updatedEdge = await _edgeBusiness.UpdateEdge(originId, destinationId, edge);
+                var updatedEdge = await _edgeBusiness.UpdateEdge(projectId, edge, originId, destinationId, edgeId);
                 return Ok(updatedEdge);
             }
             catch (Exception exc)
             {
-                var message = $"An error occurred while updating edge with origin {originId} and destination {destinationId}: {exc}";
+                var message = $"An error occurred while updating edge: {exc}";
                 NLog.LogManager.GetCurrentClassLogger().Error(message);
                 return StatusCode(StatusCodes.Status500InternalServerError, message);
             }
         }
 
+        /// <summary>
+        /// Deletes a specific edge by its ID or origin/destination.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to which the edge belongs.</param>
+        /// <param name="edgeId">The ID of the edge to delete</param>
+        /// <param name="originId">The origin ID of the edge to delete if edgeID is not present.</param>
+        /// <param name="destinationId">The destination ID of the edge if edgeID is not present.</param>
+        /// <param name="force">Indicates whether to force delete the edge if true.</param>
+        /// <returns>A message stating the edge was successfully deleted.</returns>
         [HttpDelete("DeleteEdge")]
         public async Task<IActionResult> DeleteEdge(
-            [FromQuery, Required] long originId, 
-            [FromQuery, Required] long destinationId)
+            long projectId,
+            [FromQuery] long edgeId,
+            [FromQuery] long originId, 
+            [FromQuery] long destinationId, 
+            [FromQuery] bool force = false)
         {
             try
             {
-                await _edgeBusiness.DeleteEdge(originId, destinationId);
+                await _edgeBusiness.DeleteEdge(projectId, edgeId, originId, destinationId, force);
                 return NoContent();
             }
             catch (Exception exc)
             {
-                var message = $"An error occurred while deleting edge with origin {originId} and destination {destinationId}: {exc}";
+                var message = $"An error occurred while deleting edge: {exc}";
                 NLog.LogManager.GetCurrentClassLogger().Error(message);
                 return StatusCode(StatusCodes.Status500InternalServerError, message);
             }
