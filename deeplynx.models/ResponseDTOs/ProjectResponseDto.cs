d9758655c6f0e85701ed586a4fa1c7ca8eb1b7a0
Jaren Brownlee <jaren.brownlee@inl.gov>
2025-06-04 13:54:25 -0500

    project dto's + comments

---
+++ deeplynx.models/ResponseDTOs/ProjectResponseDto.cs
@@ -0,0 +1,13 @@
+namespace deeplynx.models;
+
+public class ProjectResponseDto
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string? Abbreviation { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? ModifiedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+}
