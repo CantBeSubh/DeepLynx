8638a27b3a81accebaed1c3cbddd1583a86d6a43
Isaac Andrew Huffman <huffia@inl434590.inel.gov>
2025-06-11 11:56:55 -0600

    deleted unnecessary exception

--- deeplynx.helpers/Exceptions/exceptions.cs
+++ deeplynx.helpers/Exceptions/exceptions.cs
@@ -4,8 +4,3 @@ public class ProjectDependencyDeletionException : Exception
 {
     public ProjectDependencyDeletionException(string message) : base(message) { }
 }
-
-public class ClassDependencyDeletionException : Exception
-{
-    public ClassDependencyDeletionException(string message) : base(message) { }
-}
