90def908b407bdc8c98c3455b0b225686894659f
combam1 <autumn.combs@inl.gov>
2025-06-19 10:04:55 -0600

    correcting styling

--- deeplynx.UI/deeplynx-v3/src/app/(home)/(routes)/projects/page.tsx
+++ deeplynx.UI/deeplynx-v3/src/app/(home)/(routes)/projects/page.tsx
@@ -163,10 +163,11 @@ const Projects = () => {
             )}
           />
         </div>
+
         <div className="w-full md:w-1/2 px-4">
           <div className="my-4 flex justify-between items-center justify-end">
             <button
-              className="btn btn-outline btn-sm btn-accent mr-2"
+              className="btn btn-outline btn-sm btn-accent mr-2 btn-secondary"
               // onClick={addWidget} // Adds new widget when clicked
             >
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -176,7 +177,7 @@ const Projects = () => {
               Customize
             </button>
             <button
-              className="btn btn-outline btn-sm btn-accent"
+              className="btn btn-outline btn-sm btn-accent btn-secondary"
               // onClick={addWidget} // Adds new widget when clicked
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
@@ -185,9 +186,11 @@ const Projects = () => {
               Widget
             </button>
           </div>
+
           <div className="card card-border bg-base-100 w-141 justify-end mb-4">
             <div className="card-body">
-              <h2 className="card-title">Links
+              <h2 className="card-title justify-between items-center">
+                Links
                 <button onClick={openModal}>
                   {/* change to add link */}
                   <svg
@@ -205,25 +208,25 @@ const Projects = () => {
                 </button>
               </h2>
               <div className="justify-center">
-                <button className="btn btn-link">
+                <button className="btn btn-link text-secondary-content">
                   Roles
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                   </svg>
                 </button>
-                <button className="btn btn-link">
+                <button className="btn btn-link text-secondary-content">
                   File Explorer
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                   </svg>
                 </button>
-                <button className="btn btn-link">
+                <button className="btn btn-link text-secondary-content">
                   Reports
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
                   </svg>
                 </button>
-                <button className="btn btn-link">
+                <button className="btn btn-link text-secondary-content">
                   Trends
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
@@ -232,11 +235,13 @@ const Projects = () => {
               </div>
             </div>
           </div>
+
           <div className="card card-border bg-base-100 w-141 justify-end mb-4">
             <div className="card-body">
               <h2 className="card-title">Data Overview</h2>
             </div>
           </div>
+
           <div className="card card-border bg-base-100 w-141 justify-end mb-4">
             <div className="card-body">
               <h2 className="card-title">Graph</h2>
