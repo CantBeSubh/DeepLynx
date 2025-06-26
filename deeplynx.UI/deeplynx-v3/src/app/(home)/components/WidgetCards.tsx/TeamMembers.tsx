dc660f53b53cc64600316f2963fc334e6927978f
combam1 <autumn.combs@inl.gov>
2025-06-26 16:40:58 -0600

    adding fake people to fake team on widget

--- deeplynx.UI/deeplynx-v3/src/app/(home)/components/WidgetCards.tsx/TeamMembers.tsx
+++ deeplynx.UI/deeplynx-v3/src/app/(home)/components/WidgetCards.tsx/TeamMembers.tsx
@@ -9,6 +9,19 @@ const TeamMembersWidget = () => {
   return (
     <div className="card-body">
       <h2 className="card-title">Team Members</h2>
+      <div className="space-x-2">
+        <p className="text-base-300 mb-2"></p>
+        {[...Array(5)].map((_, i) => (
+          <div key={i} className="avatar inline-block">
+            <div className="w-10 rounded-full">
+              <img
+                src={`https://i.pravatar.cc/150?img=${i + 1}`}
+                alt="avatar"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
