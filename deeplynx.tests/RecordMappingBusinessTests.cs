140f9eb5c46c71953295adb4cc6a263373e25720
Isaac Huffman <isaac.huffman@inl.gov>
2025-07-07 08:44:19 -0600

    got rid of tag test since it will be a many to many soon

--- deeplynx.tests/RecordMappingBusinessTests.cs
+++ deeplynx.tests/RecordMappingBusinessTests.cs
@@ -308,37 +308,6 @@ public class RecordMappingBusinessTests : IntegrationTestBase
     }
     
     [Fact]
-    public async Task RecordMappingArchived_WhenTagArchived()
-    {
-        await SeedTestDataAsync();
-        var beforeArchive = DateTime.UtcNow;
-        var recordMapping1 = new RecordMapping
-        {
-            RecordParams = "{\"hello\":\"world1\"}",
-            ClassId = cid,
-            TagId = tid,
-            ProjectId = pid,
-            CreatedAt = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified),
-            CreatedBy = null,
-        };
-        Context.RecordMappings.Add(recordMapping1);
-        await Context.SaveChangesAsync();
-        
-        var deletedResult = await _tagBusiness.ArchiveTag(pid, tid);
-        Assert.True(deletedResult);
-        
-        // procedure is not traced by entity framework
-        //this forces EF to sync to db on next query
-        Context.ChangeTracker.Clear();
-        
-        var archivedRecordMapping = await Context.RecordMappings.FindAsync(recordMapping1.Id);
-        Assert.NotNull(archivedRecordMapping);
-        Assert.NotNull(archivedRecordMapping.ArchivedAt);
-        Assert.True(archivedRecordMapping.ArchivedAt >= beforeArchive);
-        Assert.True(archivedRecordMapping.ArchivedAt <= DateTime.UtcNow); 
-    }
-    
-    [Fact]
     public async Task RecordMappingArchived_WhenClassArchived()
     {
         await SeedTestDataAsync();
