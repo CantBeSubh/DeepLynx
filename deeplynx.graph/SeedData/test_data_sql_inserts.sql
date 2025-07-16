c1120b595043661cf641f04c3b7dbe9c118fc364
ahmon-embaye <Ahmon.Embaye@inl.gov>
2025-07-16 14:05:34 -0600

    Added files to seed data for kuzu tests.

---
+++ deeplynx.graph/SeedData/test_data_sql_inserts.sql
@@ -0,0 +1,24 @@
+SET search_path TO deeplynx;
+
+INSERT INTO historical_records (properties, data_source_id, data_source_name, project_name, class_name, original_id, name, tags, project_id) VALUES
+('{"name": "series", "genre": "country", "year": 1997}'::jsonb, 6, 6, 2, 'musician', 1, 'series', '["{tag1}"]', 1),
+('{"name": "possible", "genre": "pop", "year": 1984}'::jsonb, 6, 6, 2, 'song', 2, 'possible', '["{tag1}"]', 1);
+
+
+INSERT INTO historical_edges (origin_id, destination_id, relationship_name, data_source_id, project_id) VALUES
+(2, 1, 'plays', 1, 1);
+
+
+INSERT INTO classes (name, project_id) VALUES
+('band', 2),
+('song', 1);
+
+INSERT INTO data_sources (name, project_id) VALUES
+('mysrc_1', 1);
+
+INSERT INTO tags (name, project_id) VALUES
+('{tag1}', 1);
+
+INSERT INTO relationships (name, origin_id, destination_id, project_id) VALUES
+('plays', 1, 2, 1);
+
