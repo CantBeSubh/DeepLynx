df1edea91dd0dd31480a031fc5359a4f94fb19df
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-07-17 07:58:37 -0600

    remove unused model

--- deeplynx.models/RequestDTOs/FilterRequestRecord.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Nodes;
-
-namespace deeplynx.models;
-
-public class FilterRequestDto
-{
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-}
