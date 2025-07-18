a6eb9c628421a6971f68bf6babd2383729259373
ahmon-embaye <Ahmon.Embaye@inl.gov>
2025-07-18 15:47:59 -0600

    Added necessary files to build the Kuzu libraries, class files, and wrapper files.

--- deeplynx.graph/SeedData/test_data_sql_inserts.sql
+++ deeplynx.graph/SeedData/test_data_sql_inserts.sql
@@ -1,17 +1,17 @@
 SET search_path TO deeplynx;
 
-INSERT INTO historical_records (properties, data_source_id, data_source_name, project_name, class_name, original_id, name, tags, project_id) VALUES
-('{"name": "series", "genre": "country", "year": 1997}'::jsonb, 6, 6, 2, 'musician', 1, 'series', '["{tag1}"]', 1),
-('{"name": "possible", "genre": "pop", "year": 1984}'::jsonb, 6, 6, 2, 'song', 2, 'possible', '["{tag1}"]', 1);
+INSERT INTO historical_records (properties, data_source_id, data_source_name, project_name, class_name, original_id, name, tags, project_id, class_id) VALUES
+('{"name": "series", "genre": "country", "year": 1997}'::jsonb, 6, 6, 2, 'musician', 1, 'series', '["{tag1}"]', 1, 1),
+('{"name": "possible", "genre": "pop", "year": 1984}'::jsonb, 6, 6, 2, 'song', 2, 'possible', '["{tag1}"]', 1, 1);
 
 
 INSERT INTO historical_edges (origin_id, destination_id, relationship_name, data_source_id, project_id) VALUES
-(2, 1, 'plays', 1, 1);
+(1, 2, 'plays', 1, 1);
 
 
 INSERT INTO classes (name, project_id) VALUES
-('band', 2),
-('song', 1);
+('musician', 1),
+('song', 2);
 
 INSERT INTO data_sources (name, project_id) VALUES
 ('mysrc_1', 1);
