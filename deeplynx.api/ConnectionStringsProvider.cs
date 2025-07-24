f996dd8af4aa5c2bb618186cfbfdd409d16dd74d
Ed R <145260780+era-zx@users.noreply.github.com>
2025-07-24 13:52:38 -0600

    initial connection strings builder class

---
+++ deeplynx.api/ConnectionStringsProvider.cs
@@ -0,0 +1,38 @@
+using DotNetEnv;
+
+using deeplynx.api;
+
+public static class ConnectionStringsProvider
+{
+    public static string GetPostgresConnectionString(IConfiguration configuration)
+    {
+        Env.Load();
+
+        var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+        var postgresUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var postgresPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+        var postgresDatabaseName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+        var postgresServer = Environment.GetEnvironmentVariable("POSTGRES_DBHOST");
+        var postgresPort = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+
+        if (!string.IsNullOrEmpty(postgresUser) &&
+            !string.IsNullOrEmpty(postgresPassword) &&
+            !string.IsNullOrEmpty(postgresDatabaseName) &&
+            !string.IsNullOrEmpty(postgresServer) &&
+            !string.IsNullOrEmpty(postgresPort))
+        {
+            NLog.LogManager.GetCurrentClassLogger().Info("Using .env Postgres connection credentials");
+            return $"User ID={postgresUser};Password={postgresPassword};Database={postgresDatabaseName};Server={postgresServer};Port={postgresPort};";
+        }
+        else if (!string.IsNullOrEmpty(defaultConnectionString))
+        {
+            NLog.LogManager.GetCurrentClassLogger().Info(".env Postgres connection variables not configured. Falling back to default postgres connection credentials");
+            return defaultConnectionString;
+        }
+        else
+        {
+            throw new InvalidOperationException("No valid database connection string was found.");
+        }
+    }
+}
