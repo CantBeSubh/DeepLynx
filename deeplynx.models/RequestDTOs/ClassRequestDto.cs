545208a36a8bd37e70aac62d289192b37f2cc462
Ed R <145260780+era-zx@users.noreply.github.com>
2025-07-28 09:24:44 -0600

    updated request dto json field names

--- deeplynx.models/RequestDTOs/ClassRequestDto.cs
+++ deeplynx.models/RequestDTOs/ClassRequestDto.cs
@@ -1,10 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace deeplynx.models;
 
 public class ClassRequestDto
 {
     [Required]
+    [JsonPropertyName("name")]
     public string Name { get; set; }
-    public string? Uuid { get; set; }
+    
+    [JsonPropertyName("description")]
     public string? Description { get; set; }
+    
+    [JsonPropertyName("uuid")]
+    public string? Uuid { get; set; }
 }
