bbae92a9b610119ecd7ae14696f2f4b9126bfd8d
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-07-31 11:17:42 -0600

    added env

--- deeplynx.UI/deeplynx-v3/src/app/lib/projects_services.ts
+++ deeplynx.UI/deeplynx-v3/src/app/lib/projects_services.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const api = axios.create({ withCredentials: true })
+export const api = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true
+})
 
 export const getAllProjects = async () => {
     try {
-        const res = await api.get(API_BASE_URL + "/projects/GetAllProjects");
+        const res = await api.get("/projects/GetAllProjects");
         return res.data;
     } catch (error) {
         console.error("API call failed:", error);
@@ -16,7 +19,7 @@ export const getAllProjects = async () => {
 
 export const getProject = async (projectId: string) => {
     try {
-        const res = await api.get(API_BASE_URL + `/projects/GetProject/${projectId}`);
+        const res = await api.get(`/projects/GetProject/${projectId}`);
         return res.data;
     } catch (error) {
         console.error("API call failed:", error);
@@ -26,7 +29,7 @@ export const getProject = async (projectId: string) => {
 
 export const getProjectStats = async (projectId: string) => {
     try {
-        const res = await api.get(API_BASE_URL + `/projects/ProjectStats/${projectId}`);
+        const res = await api.get(`/projects/ProjectStats/${projectId}`);
         return res.data;
     } catch (error) {
         console.error("API call failed:", error)
@@ -39,7 +42,7 @@ export const createProject = async (data: {
     abbreviation: string | null;
     description: string | null;
 }) => {
-    const res = await api.post(API_BASE_URL + "/projects/CreateProject", data, {
+    const res = await api.post("/projects/CreateProject", data, {
         headers: { "Content-Type": "application/json" },
     });
     return res.data;
--- deeplynx.UI/deeplynx-v3/src/app/lib/record_services.ts
+++ deeplynx.UI/deeplynx-v3/src/app/lib/record_services.ts
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const api = axios.create({ withCredentials: true });
-
+export const api = axios.create({
+    baseURL: API_BASE_URL,
+    withCredentials: true
+})
 export const getAllRecords = async (projectId: string) => {
     console.log("Project ID:", projectId)
     try {
-        const res = await api.get(API_BASE_URL + `/projects/${projectId}/records/GetAllRecords`);
+        const res = await api.get(`/projects/${projectId}/records/GetAllRecords`);
         return res.data;
     } catch (error) {
         console.error("API call failed:", error);
