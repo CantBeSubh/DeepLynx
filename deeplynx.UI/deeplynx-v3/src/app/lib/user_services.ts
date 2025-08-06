610087e0446d4b62f26ee28c0b8ce4e1a62921bd
ahmon-embaye <Ahmon.Embaye@inl.gov>
2025-08-06 16:33:13 -0600

    Add client_secret and auth_secret env variables

--- deeplynx.UI/deeplynx-v3/src/app/lib/user_services.ts
+++ deeplynx.UI/deeplynx-v3/src/app/lib/user_services.ts
@@ -4,6 +4,9 @@ console.log('All environment variables:', {
     OKTA_ISSUER: process.env.NEXT_PUBLIC_OKTA_ISSUER,
     OKTA_CLIENT_ID: process.env.NEXT_PUBLIC_OKTA_CLIENT_ID,
     API_URL: process.env.NEXT_PUBLIC_API_URL,
+    OKTA_CLIENT_SECRET: process.env.NEXT_PUBLIC_OKTA_CLIENT_SECRET,
+    AUTH_SECRET: process.env.NEXT_PUBLIC_AUTH_SECRET,
+    REDIRECT_LINK: process.env.NEXT_PUBLIC_REDIRECT_LINK,
     NODE_ENV: process.env.NODE_ENV,
 });
 
