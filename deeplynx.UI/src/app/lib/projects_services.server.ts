3a15508a450f4ef1ba65d7009c27c3c23876c692
ahmon-embaye <Ahmon.Embaye@inl.gov>
2025-08-18 13:21:15 -0600

    Changed name to service not server

--- deeplynx.UI/src/app/lib/projects_services.server.ts
+++ deeplynx.UI/src/app/lib/projects_services.server.ts
@@ -2,8 +2,8 @@
 import "server-only";
 import type { FileViewerTableRow } from "@/app/(home)/types/types";
 
-const BASE = process.env.BACKEND_BASE_URL!;
-const SERVICE_TOKEN = process.env.SERVICE_TOKEN || "";
+const BASE = process.env.BACKEND_BASE_URL || "BASE URL IS NOT DEFINED";
+const SERVICE_TOKEN = process.env.SERVICE_TOKEN || "xxx";
 
 console.log("BACKEND BASE URL:",process.env.BACKEND_BASE_URL)
 console.log("SERVICE TOKEN:",process.env.SERVICE_TOKEN)
