4fae9c6b0c90000e39fca3cf03e4adb9ed87d5f6
ahmon-embaye <Ahmon.Embaye@inl.gov>
2025-08-19 10:58:00 -0600

    Added edge_services client

---
+++ deeplynx.UI/src/app/lib/edge_services.client.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    withCredentials: true
+});
+
+export const deleteEdge = async (projectId: number, edgeId: string | null, originId: string, destinationId: string) => {
+    try {
+        let queryParams = new URLSearchParams();
+        if (edgeId) queryParams.append('edgeId', edgeId);
+        if (originId) queryParams.append('originId', originId);
+        if (destinationId) queryParams.append('destinationId', destinationId);
+
+        const res = await api.delete(`/projects/${projectId}/edges/DeleteEdge`, {
+            params: queryParams
+        });
+        return res.data;
+    } catch (error) {
+        console.error("API call failed:", error);
+        throw error;
+    }
+};
+
+export const getEdge = async (projectId: number, edgeId: string | null, originId: string, destinationId: string) => {
+    try {
+        let queryParams = new URLSearchParams();
+        if (edgeId) queryParams.append('edgeId', edgeId);
+        if (originId) queryParams.append('originId', originId);
+        if (destinationId) queryParams.append('destinationId', destinationId);
+
+        const res = await api.get(`/projects/${projectId}/edges/GetEdge`, {
+            params: queryParams
+        });
+        return res.data;
+    } catch (error) {
+        console.error("API call failed:", error);
+        throw error;
+    }
+};
