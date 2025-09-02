b383d3db03bafca2307aaa2c21eb1913ea934005
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-09-02 11:54:31 -0600

    Env updates

--- deeplynx.UI/auth.ts
+++ deeplynx.UI/auth.ts
@@ -1,23 +1,19 @@
 import NextAuth from "next-auth";
-import OktaProvider from "next-auth/providers/okta"
+import Okta from "next-auth/providers/okta";
 
-export const {
-    handlers,
-    auth,
-    signIn,
-    signOut,
-} = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [
-        OktaProvider({
-            clientId: process.env.NEXT_PUBLIC_OKTA_CLIENT_ID!,
-            clientSecret: process.env.NEXT_PUBLIC_OKTA_CLIENT_SECRET!,
-            issuer: process.env.NEXT_PUBLIC_OKTA_ISSUER,
+        Okta({
+            clientId: process.env.OKTA_CLIENT_ID!,
+            clientSecret: process.env.OKTA_CLIENT_SECRET!,
+            issuer: process.env.OKTA_ISSUER,
             authorization: {
                 params: {
-                    scope: "openid profile",
-                    redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_LINK,
+                    scope: "openid profile email",
                 },
             },
-        })
+        }),
     ],
 });
+
+export const { GET, POST } = handlers; 
