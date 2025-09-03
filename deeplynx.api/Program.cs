5d3026382c39d40dd5e382662a86298af5508b5b
keaton-flake <keaton.flake@inl.gov>
2025-09-03 15:43:12 -0600

    merged changes

--- deeplynx.api/Program.cs
+++ deeplynx.api/Program.cs
@@ -122,6 +122,7 @@ try
     builder.Services.AddTransient<IHistoricalRecordBusiness, HistoricalRecordBusiness>();
     builder.Services.AddTransient<IHistoricalEdgeBusiness, HistoricalEdgeBusiness>();
     builder.Services.AddTransient<IEventBusiness, EventBusiness>();
+    builder.Services.AddTransient<ISubscriptionBusiness, SubscriptionBusiness>();
     builder.Services.AddTransient<FileBusiness>();
     builder.Services.AddTransient<FileFilesystemBusiness>();
     builder.Services.AddTransient<FileAzureBusiness>();
@@ -241,10 +242,15 @@ try
                     Description =
                         "Manages user-related operations, including user creation, updates, retrieval, and authentication processes."
                 },
-                new OpenApiTag
-                {
-                    Name = "Event",
-                    Description = "Handles Event fetching by project and user subscriptions."
+                new OpenApiTag 
+                { 
+                    Name = "Event", 
+                    Description = "Handles Event fetching by project and user subscriptions." 
+                },
+                new OpenApiTag 
+                { 
+                    Name = "Subscription", 
+                    Description = "Handles operations related to subscription creation, retrieval, and deletion." 
                 },
                 new OpenApiTag
                 {
--- deeplynx.models/RequestDTOs/UpdateDTOs/UpdateSubscriptionRequestDto.cs
+++ deeplynx.models/RequestDTOs/UpdateDTOs/UpdateSubscriptionRequestDto.cs
@@ -3,9 +3,9 @@ namespace deeplynx.models;
 public class UpdateSubscriptionRequestDto
 {
     public long Id { get; set; }
-    public long ActionId {get; set;}
-    public string Operation {get; set;}
-    public long DataSourceId {get; set;}
-    public string EntityType {get; set;}
-    public  long EntityId {get; set;}
+    public long? ActionId {get; set;}
+    public string? Operation {get; set;}
+    public long? DataSourceId {get; set;}
+    public string? EntityType {get; set;}
+    public long? EntityId {get; set;}
 }
