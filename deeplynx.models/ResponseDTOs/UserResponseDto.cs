7fb17650de495c42ba2c739742c6e4067be672eb
Jaren Brownlee <jaren.brownlee@inl.gov>
2025-09-09 15:24:32 -0500

    tag changes

--- deeplynx.models/ResponseDTOs/UserResponseDto.cs
+++ deeplynx.models/ResponseDTOs/UserResponseDto.cs
@@ -5,6 +5,5 @@ public class UserResponseDto
     public long Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public DateTime? ArchivedAt { get; set; }
     public bool IsSysAdmin { get; set; }
 }
