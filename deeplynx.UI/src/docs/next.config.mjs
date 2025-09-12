7a2db2db437988a7a914ad5ab8304dc1ce02ce06
Ed R <145260780+era-zx@users.noreply.github.com>
2025-09-12 17:29:43 -0600

    nexus docs subproject

---
+++ deeplynx.UI/src/docs/next.config.mjs
@@ -0,0 +1,17 @@
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+  search: true,
+  search: { codeblocks: true },
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+})
---
+++ deeplynx.UI/src/docs/src/content/_meta.ts
@@ -0,0 +1,13 @@
+export default {
+  overview: {
+    title: "How it works"
+  },
+  users: '',
+  developers: '',
+  about: '',
+  contact: {
+    title: 'Contact us',
+    type: 'page',
+    href: 'https://example.com/contact'
+  }
+}
