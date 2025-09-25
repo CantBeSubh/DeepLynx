925f3d63af3c76101b5390414518b2e61f27b1e8
Ed R. <145260780+era-zx@users.noreply.github.com>
2025-09-25 13:44:34 -0600

    updated happy emoji

--- deeplynx.docs/src/app/layout.tsx
+++ deeplynx.docs/src/app/layout.tsx
@@ -9,7 +9,7 @@ import { ReactNode } from 'react'
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/jmetadata
 //}
  
-const banner = <Banner storageKey="banner-key0">Welcome to the release of Nexus Docs 1.0 🎉</Banner>
+const banner = <Banner storageKey="banner-key0">Welcome to the release of Nexus Docs 1.0 🎉😄</Banner>
 const navbar = (
   <Navbar
     logo={
