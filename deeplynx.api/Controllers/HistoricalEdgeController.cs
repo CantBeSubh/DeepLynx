63d82573340f1905b797fc3b3d041df3d1ea3ccc
Jason Kuipers <jason.kuipers@inl.gov>
2025-09-26 10:26:15 -0600

    adding custom authorize attribute

--- deeplynx.api/Controllers/HistoricalEdgeController.cs
+++ deeplynx.api/Controllers/HistoricalEdgeController.cs
@@ -1,3 +1,4 @@
+using deeplynx.helpers;
 using Microsoft.AspNetCore.Mvc;
 using deeplynx.interfaces;
 using deeplynx.models;
@@ -6,6 +7,7 @@ namespace deeplynx.api.Controllers
 {
     [ApiController]
     [Route("api/projects/{projectId}/records/historical")]
+    [NexusAuthorize]
     public class HistoricalEdgeController : ControllerBase
     {
         private readonly IHistoricalEdgeBusiness _historicalEdgeBusiness;
