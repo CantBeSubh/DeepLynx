18abc6145d743f3ecb40e67bc763314eaa0aabd7
Ed R. <145260780+era-zx@users.noreply.github.com>
2025-09-26 11:31:50 -0600

    base path arg from run to build time

--- deeplynx.docs/next.config.mjs
+++ deeplynx.docs/next.config.mjs
@@ -1,18 +1,16 @@
 import nextra from 'nextra'
 
-// Set up Nextra with its configuration
 const withNextra = nextra({
-  // Add Nextra-specific options here
   search: true,
   search: { codeblocks: true },
 })
 
-// Export the final Next.js config with Nextra included
 export default withNextra({
-  // Add regular Next.js options here
+// Add regular Next.js options here
   reactStrictMode: true,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  trailingSlash: true, 
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.output.publicPath = `${process.env.NEXT_PUBLIC_BASE_PATH || ''}/_next/`;
