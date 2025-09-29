9b939026526962edea8af3ece9c70fb2c20c89e1
keaton-flake <keaton.flake@inl.gov>
2025-09-29 12:48:56 -0600

    start

---
+++ deeplynx.hubs/EventNotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace deeplynx.hubs
+{
+    public class EventNotificationHub : Hub
+    {
+        public async Task SendNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+    }
+}
