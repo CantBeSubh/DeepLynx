6bb4c2ce3e8744cee016d847c16eeeb0505b99c2
combam1 <autumn.combs@inl.gov>
2025-10-01 16:29:00 -0600

    removing comments

--- deeplynx.UI/src/app/(home)/components/MemberSettingsTable/MemberModals/AddSysUser.tsx
+++ deeplynx.UI/src/app/(home)/components/MemberSettingsTable/MemberModals/AddSysUser.tsx
@@ -26,39 +26,7 @@ const AddSysUser = ({ isOpen, onClose }: AddSysUserProps) => {
     onClose();
   };
 
-//   return (
-//     <>
-//       {isOpen && (
-//         <dialog className="modal modal-open">
-//           <div className="modal-box max-w-lg">
-//             <h3 className="font-bold text-lg mb-4 text-neutral">
-//               {t.translations.ADD_NEW_USER}
-//             </h3>
-//             {/* Form for adding a new member and selecting their role*/}
-//             <form method="dialog" className="flex flex-col gap-4">
-//                 <input
-//                 type="text"
-//                 placeholder="Email"
-//                 className="input input-primary w-full"
-//                 />
-//             </form>
-//             {/* Modal Action Buttons */}
-//             <div className="modal-action">
-//                 <button className="btn" onClick={onClose}>
-//                     {t.translations.CANCEL}
-//                 </button>
-//                 <button className="btn btn-primary" onClick={onClose}>
-//                     {t.translations.INVITE}
-//                 </button>
-//             </div>
-//           </div>
-//         </dialog>
-//       )}
-//     </>
-//   );
-// };
-
- return (
+  return (
     <>
       {isOpen && (
         <dialog className="modal modal-open">
