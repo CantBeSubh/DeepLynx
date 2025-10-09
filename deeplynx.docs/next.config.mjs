bdd6ea5321069972a7d5ee4a1901378af61b1b5d
Ed R. <145260780+era-zx@users.noreply.github.com>
2025-10-09 15:38:43 -0600

    test string path

--- deeplynx.docs/next.config.mjs
+++ deeplynx.docs/next.config.mjs
@@ -4,7 +4,7 @@ const withNextra = nextra({
   search: { codeblocks: true }
 })
 
-const basePath = process.env.NEXT_PUBLIC_DOCS_PATH || '';
+const basePath = process.env.NEXT_PUBLIC_DOCS_PATH ? "/docs" : '';
 
 export default withNextra({
 // Add regular Next.js options here
