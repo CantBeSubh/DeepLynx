3b631d50c72a603eb5094077c8209e3faf0ff41b
Ed R. <145260780+era-zx@users.noreply.github.com>
2025-10-09 12:27:35 -0600

    simplified nextra config

--- deeplynx.docs/next.config.mjs
+++ deeplynx.docs/next.config.mjs
@@ -4,16 +4,11 @@ const withNextra = nextra({
   search: { codeblocks: true }
 })
 
+const basePath = process.env.NEXT_PUBLIC_DOCS_PATH || '';
+
 export default withNextra({
 // Add regular Next.js options here
   reactStrictMode: true,
-  assetPrefix: process.env.NEXT_PUBLIC_DOCS_PATH || '',
-  basePath: process.env.NEXT_PUBLIC_DOCS_PATH || '',
   trailingSlash: true, 
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.output.publicPath = `${process.env.NEXT_PUBLIC_DOCS_PATH || ''}/_next/`;
-    }
-    return config;
-  },
+  basePath
 })
