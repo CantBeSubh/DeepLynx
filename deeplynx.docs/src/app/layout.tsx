a71afea344c17f67d861b49302242eb762b41b47
Ed R. <145260780+era-zx@users.noreply.github.com>
2025-10-09 18:18:20 -0600

    testing function scoped var

--- deeplynx.docs/src/app/layout.tsx
+++ deeplynx.docs/src/app/layout.tsx
@@ -48,8 +48,7 @@ const navbar = (
 
 const footer = <Footer>{new Date().getFullYear()} © Idaho National Laboratory.</Footer>
 
-//const basePath = process.env.NEXT_PUBLIC_DOCS_PATH || ''
-const basePath = '/docs'
+const basePath = process.env.NEXT_PUBLIC_DOCS_PATH || ''
 const faviconPath = `${basePath}/cropped-lynx-white.png` 
 
 export default async function RootLayout({ children }: { children: ReactNode }) { 
