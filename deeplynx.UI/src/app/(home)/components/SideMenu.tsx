cec661c31a89c1e3e948fc3d82e9406d0768c08a
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-10-20 14:21:47 -0600

    stupid button

--- deeplynx.UI/src/app/(home)/components/SideMenu.tsx
+++ deeplynx.UI/src/app/(home)/components/SideMenu.tsx
@@ -220,8 +220,8 @@ const SideMenu: React.FC<SideMenuProps> = ({ onToggle }) => {
         {/* BUG ISSUE: When ever a project is not selected all middle menu items should be disabled. But the bug is, when the last 4 menu items are clicked it activates the middle menu items. */}
         <div className="mt-auto">
           <ul>
-            <li className="mt-2"><a href={process.env.NEXT_PUBLIC_DOCS_PATH}><QuestionMarkCircleIcon className="size-6" />
-              {!isCollapsed && <p className="ml-2">{t.translations.HELP}</p>}</a></li>
+            <li className="mt-2"><button><a href={process.env.NEXT_PUBLIC_DOCS_PATH}><QuestionMarkCircleIcon className="size-6" />
+              {!isCollapsed && <p className="ml-2">{t.translations.HELP}</p>}</a></button></li>
             {/* <li className="mt-2">
               <Link
                 href="#"
