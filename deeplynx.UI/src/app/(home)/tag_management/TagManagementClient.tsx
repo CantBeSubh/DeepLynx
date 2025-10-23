dd6b3820654b74332117b9727b54f811b3b4c234
Daniel Uribe <daniel.uribeporfirio@inl.gov>
2025-10-23 11:43:46 -0600

    Left column complete

--- deeplynx.UI/src/app/(home)/tag_management/TagManagementClient.tsx
+++ deeplynx.UI/src/app/(home)/tag_management/TagManagementClient.tsx
@@ -18,23 +18,44 @@ const TagManagementClient = ({
   const [selectedProjects, setSelectedProjects] = useState<string[]>(
     initialSelectedProjects
   );
+  const [selectedMenuItem, setSelectedMenuItems] = useState("Search Tags");
+
+  const menuItems = ["Search Tags", "Create Tag", "Attach Tags", "Edit Tags"];
   return (
     <div>
-      <div className="flex justify-between items-center bg-base-200/40 pl-12 py-2 pb-4">
-        <div>
-          <h1 className="text-2xl font-bold text-info-content">
-            Tag Management
-          </h1>
-          <ProjectDropdown
-            projects={projects}
-            onSelectionChange={setSelectedProjects}
-            defaultSelected={
-              initialSelectedProjects.length
-                ? initialSelectedProjects
-                : undefined
-            }
-          />
+      {/* Header */}
+      <div className="items-center bg-base-200/40 pl-12 py-2 pb-4">
+        <h1 className="text-2xl font-bold text-info-content">Tag Management</h1>
+        <ProjectDropdown
+          projects={projects}
+          onSelectionChange={setSelectedProjects}
+          defaultSelected={
+            initialSelectedProjects.length ? initialSelectedProjects : undefined
+          }
+        />
+      </div>
+
+      {/* Content */}
+      <div className="grid grid-cols-[20%_40%_40%] p-6">
+        <div className="card shadow-xl rounded-lg p-6 mr-6">
+          <ul>
+            {menuItems.map((item) => (
+              <li
+                key={item}
+                onClick={() => setSelectedMenuItems(item)}
+                className={`cursor-pointer px-4 py-2 rounded-lg transition-colors font-bold ${
+                  selectedMenuItem === item
+                    ? "bg-info/50 text-info-content"
+                    : "hover:bg-base-200"
+                }`}
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
         </div>
+        <div className="card shadow-xl rounded-lg p-6 mr-6">Tags</div>
+        <div className="card shadow-xl rounded-lg p-6">Records</div>
       </div>
     </div>
   );
