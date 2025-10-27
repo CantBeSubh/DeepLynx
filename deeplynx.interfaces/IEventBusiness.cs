80d115d553b2038f9e8753edddad725fe9b84fda
keaton-flake <keaton.flake@inl.gov>
2025-10-27 08:38:02 -0600

    testing pagination

--- deeplynx.interfaces/IEventBusiness.cs
+++ deeplynx.interfaces/IEventBusiness.cs
@@ -1,13 +1,12 @@
 using deeplynx.models;
-using deeplynx.datalayer.Models;
 namespace deeplynx.interfaces;
 
 public interface IEventBusiness
 {
-    Task<PaginatedResponse<EventResponseDto>> GetAllEvents(EventFilterRequestDTO? filterDto);
+    Task<List<EventResponseDto>> GetAllEvents(EventFilterRequestDTO? filterDto);
+    Task<PaginatedResponse<EventResponseDto>> GetAllEventsPaginated(EventFilterRequestDTO? filterDto);
+    Task<PaginatedResponse<EventResponseDto>> GetAllEventsByUserPaginated(EventFilterRequestDTO? filterDto);
     Task<List<EventResponseDto>> GetAllEventsByUserProjectSubscriptions(long userId, long projectId);
-    Task<List<EventResponseDto>> GetAllEventsByUser(EventFilterRequestDTO? filterDto);
     Task<EventResponseDto> CreateEvent(CreateEventRequestDto dto);
-    Task<List<EventResponseDto>> BulkCreateEvents(long projectId, List<CreateEventRequestDto> events
-    );
+    Task<List<EventResponseDto>> BulkCreateEvents(long projectId, List<CreateEventRequestDto> events);
 }
