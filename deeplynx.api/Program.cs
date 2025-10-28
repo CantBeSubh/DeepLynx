ff11e99dcca02eb9ed882319cf16e02d4c63b386
Jaren Brownlee <jaren.brownlee@inl.gov>
2025-10-28 15:35:32 -0500

    add dash wildcard origin

--- deeplynx.api/Program.cs
+++ deeplynx.api/Program.cs
@@ -68,7 +68,8 @@ try
                     "https://*.svc.cluster.local",
                     "http://*.svc.cluster.local",
                     "https://deeplynx.*.inl.gov",  // Matches deeplynx.dev.inl.gov, deeplynx.acc.inl.gov, etc.
-                    "https://deeplynx.inl.gov")
+                    "https://deeplynx.inl.gov",
+                    "https://deeplynx-*.inl.gov")  // Matches deeplynx-test.inl.gov, as well as future "deeplynx-thing" namespaces
                 .SetIsOriginAllowedToAllowWildcardSubdomains()
                 .AllowAnyMethod()
                 .AllowAnyHeader()
