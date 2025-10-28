4619a437427c731a12df39ad63ee35dc889b1d14
Natalie Hergesheimer <natalie.hergesheimer@inl.gov>
2025-10-28 15:56:05 -0600

    added check for archived permission and role

--- deeplynx.helpers/Middleware/RolePermissionService.cs
+++ deeplynx.helpers/Middleware/RolePermissionService.cs
@@ -59,18 +59,21 @@ public class RolePermissionService : IRolePermissionService
         //check for whether a user has permission to an action/resource within a project through group membership
         var hasGroupPermission = _dbContext.Database
             .SqlQuery<bool>($@"
-                SELECT EXISTS (
+               SELECT EXISTS (
                     SELECT 1
                     FROM deeplynx.users u
                     LEFT JOIN deeplynx.group_users gu ON gu.user_id = u.id
                     LEFT JOIN deeplynx.groups g ON gu.group_id = g.id
                     LEFT JOIN deeplynx.project_members pm ON (pm.user_id = u.id OR pm.group_id = g.id)
+                    LEFT JOIN deeplynx.roles r ON r.id = pm.role_id
                     LEFT JOIN deeplynx.role_permissions rp ON rp.role_id = pm.role_id
                     LEFT JOIN deeplynx.permissions perm ON rp.permission_id = perm.id
                     WHERE u.id = {userId}
                       AND pm.project_id = {projectId}
                       AND perm.resource = {resource}
                       AND perm.action = {action}
+                      AND r.is_archived = false
+                      AND perm.is_archived = false
                 ) AS has_permission")
             .AsEnumerable()
             .FirstOrDefault();
