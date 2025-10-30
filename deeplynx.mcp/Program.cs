64e953532f8817a12f6e923aa6887877b9f91602
Isaac Huffman <isaac.huffman@inl.gov>
2025-10-30 09:35:28 -0600

    made a simple remote http server

--- deeplynx.mcp/Program.cs
+++ deeplynx.mcp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program.cs
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMcpServer()
+    .WithHttpTransport()
+    .WithToolsFromAssembly();
+var app = builder.Build();
+
+app.MapMcp();
+
+app.Run("http://localhost:3001");
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Returns the current date as a formatted string.")]
+    public static string CurrentDate(string message) => DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    
+    [McpServerTool, Description("Add two messages together and returns the message back to the client.")]
+    public static string Echo2(string message1, string message2) => $"hello {message1} {message2}";
+    
+}
