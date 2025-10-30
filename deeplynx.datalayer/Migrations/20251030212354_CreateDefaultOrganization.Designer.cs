b7993bfd519bc054cba0c45e5994ff3da21096bb
combam1 <autumn.combs@inl.gov>
2025-10-30 16:37:46 -0600

    adding migration

--- deeplynx.datalayer/Migrations/20251030212354_CreateDefaultOrganization.Designer.cs
+++ deeplynx.datalayer/Migrations/20251030212354_CreateDefaultOrganization.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using deeplynx.datalayer.Models;
@@ -12,9 +13,11 @@ using deeplynx.datalayer.Models;
 namespace deeplynx.datalayer.Migrations
 {
     [DbContext(typeof(DeeplynxContext))]
-    partial class DeeplynxContextModelSnapshot : ModelSnapshot
+    [Migration("20251030212354_CreateDefaultOrganization")]
+    partial class CreateDefaultOrganization
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
