e05ee37cc18a61acca2fb4193e1883d870441e1c
Drew Rizk <drew.rizk@inl.gov>
2025-11-20 10:49:54 -0700

    redundant index

--- deeplynx.datalayer/Models/ApiKey.cs
+++ deeplynx.datalayer/Models/ApiKey.cs
@@ -1,33 +1,25 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace deeplynx.datalayer.Models;
 
 [Table("api_keys", Schema = "deeplynx")]
-[Index("ApplicationId", Name = "idx_api_keys_application_id")]
-public partial class ApiKey
+public class ApiKey
 {
-    [Key]
-    [Column("id")]
-    public long Id { get; set; }
+    [Key] [Column("id")] public long Id { get; set; }
 
-    [Column("user_id")]
-    public long UserId { get; set; }
-    
-    [Column("secret")]
-    public string Secret { get; set; }
-    
-    [Column("key")]
-    public string Key { get; set; }
-    
-    [Column("application_id")]
-    public long? ApplicationId { get; set; }
+    [Column("user_id")] public long UserId { get; set; }
+
+    [Column("secret")] public string Secret { get; set; }
+
+    [Column("key")] public string Key { get; set; }
+
+    [Column("application_id")] public long? ApplicationId { get; set; }
 
     [ForeignKey("UserId")]
     [InverseProperty("ApiKeys")]
     public virtual User User { get; set; } = null!;
-    
+
     [ForeignKey("ApplicationId")]
     [InverseProperty("ApiKeys")]
     public virtual OauthApplication? OauthApplication { get; set; }
