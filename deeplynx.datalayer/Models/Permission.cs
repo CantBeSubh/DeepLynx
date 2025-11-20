f3c2d64649118e99a728f2a6f149cd453d82c626
Drew Rizk <drew.rizk@inl.gov>
2025-11-20 13:00:23 -0700

    permissions r weird

--- deeplynx.datalayer/Models/Permission.cs
+++ deeplynx.datalayer/Models/Permission.cs
@@ -4,44 +4,32 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace deeplynx.datalayer.Models;
 
 [Table("permissions", Schema = "deeplynx")]
-public partial class Permission
+public class Permission
 {
-    [Key]
-    [Column("id")]
-    public long Id { get; set; }
+    [Key] [Column("id")] public long Id { get; set; }
 
-    [Column("name")]
-    public string Name { get; set; } = null!;
+    [Column("name")] public string Name { get; set; } = null!;
 
-    [Column("description")]
-    public string? Description { get; set; }
+    [Column("description")] public string? Description { get; set; }
 
-    [Column("action")]
-    public string Action { get; set; } = null!;
+    [Column("action")] public string Action { get; set; } = null!;
 
-    [Column("resource")]
-    public string? Resource { get; set; }
+    [Column("resource")] public string? Resource { get; set; }
 
-    [Column("is_default")]
-    public bool IsDefault { get; set; } = false;
+    [Column("is_default")] public bool IsDefault { get; set; } = false;
 
-    [Column("label_id")]
-    public long? LabelId { get; set; }
+    [Column("label_id")] public long? LabelId { get; set; }
 
-    [Column("project_id")]
-    public long? ProjectId { get; set; }
+    [Column("project_id")] public long? ProjectId { get; set; }
 
-    [Column("organization_id")]
-    public long OrganizationId { get; set; }
+    [Column("organization_id")] public long? OrganizationId { get; set; }
 
-    [Column("last_updated_by")]
-    public long? LastUpdatedBy { get; set; }
+    [Column("last_updated_by")] public long? LastUpdatedBy { get; set; }
 
     [Column("last_updated_at", TypeName = "timestamp without time zone")]
     public DateTime LastUpdatedAt { get; set; }
 
-    [Column("is_archived")]
-    public bool IsArchived { get; set; }
+    [Column("is_archived")] public bool IsArchived { get; set; }
 
     [ForeignKey("LabelId")]
     [InverseProperty("Permissions")]
@@ -53,12 +41,12 @@ public partial class Permission
 
     [ForeignKey("OrganizationId")]
     [InverseProperty("Permissions")]
-    public virtual Organization Organization { get; set; } = null!;
+    public virtual Organization? Organization { get; set; }
 
     [ForeignKey("PermissionId")]
     [InverseProperty("Permissions")]
     public virtual ICollection<Role> Roles { get; set; } = new List<Role>();
-    
+
     [InverseProperty("LastUpdatedPermissions")]
     public virtual User? LastUpdatedByUser { get; set; }
 }
