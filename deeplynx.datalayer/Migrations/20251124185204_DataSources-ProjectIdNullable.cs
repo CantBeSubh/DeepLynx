f3bc3477c7b1c5e7215dfd6b15e060e73230a9f2
Ed Ramos <edward.ramos@inl.gov>
2025-11-24 11:57:23 -0700

    migrate with cascaded project deletion

---
+++ deeplynx.datalayer/Migrations/20251124185204_DataSources-ProjectIdNullable.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace deeplynx.datalayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSourcesProjectIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "data_sources_project_id_fkey",
+                schema: "deeplynx",
+                table: "data_sources");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "project_id",
+                schema: "deeplynx",
+                table: "data_sources",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "data_sources_project_id_fkey",
+                schema: "deeplynx",
+                table: "data_sources",
+                column: "project_id",
+                principalSchema: "deeplynx",
+                principalTable: "projects",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "data_sources_project_id_fkey",
+                schema: "deeplynx",
+                table: "data_sources");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "project_id",
+                schema: "deeplynx",
+                table: "data_sources",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "data_sources_project_id_fkey",
+                schema: "deeplynx",
+                table: "data_sources",
+                column: "project_id",
+                principalSchema: "deeplynx",
+                principalTable: "projects",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade); //no-opted with above up application
+        }
+    }
+}
