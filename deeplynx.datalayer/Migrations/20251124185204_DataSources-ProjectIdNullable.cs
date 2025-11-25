cf518fa08bb5f545c3785b99fd0b79ada37ed3cc
Ed Ramos <edward.ramos@inl.gov>
2025-11-24 17:08:29 -0700

    add indexes

--- deeplynx.datalayer/Migrations/20251124185204_DataSources-ProjectIdNullable.cs
+++ deeplynx.datalayer/Migrations/20251124185204_DataSources-ProjectIdNullable.cs
@@ -23,6 +23,23 @@ namespace deeplynx.datalayer.Migrations
                 nullable: true,
                 oldClrType: typeof(long),
                 oldType: "bigint");
+            
+            migrationBuilder.CreateIndex(
+                name: "idx_data_sources_org_name_unique",
+                schema: "deeplynx",
+                table: "data_sources",
+                columns: new[] { "organization_id", "name" },
+                unique: true,
+                filter: "project_id IS NULL");
+
+            // Unique when project-scoped
+            migrationBuilder.CreateIndex(
+                name: "idx_data_sources_org_project_name_unique",
+                schema: "deeplynx",
+                table: "data_sources",
+                columns: new[] { "organization_id", "project_id", "name" },
+                unique: true,
+                filter: "project_id IS NOT NULL");
 
             migrationBuilder.AddForeignKey(
                 name: "data_sources_project_id_fkey",
@@ -40,6 +57,16 @@ namespace deeplynx.datalayer.Migrations
         {
             migrationBuilder.DropForeignKey(
                 name: "data_sources_project_id_fkey",
+                schema: "deeplynx",
+                table: "data_sources");
+            
+            migrationBuilder.DropIndex(
+                name: "idx_data_sources_org_name_unique",
+                schema: "deeplynx",
+                table: "data_sources");
+
+            migrationBuilder.DropIndex(
+                name: "idx_data_sources_org_project_name_unique",
                 schema: "deeplynx",
                 table: "data_sources");
 
