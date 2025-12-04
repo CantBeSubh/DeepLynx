cdc0249c897418f07724ce86c1fff1435bec65da
Daniel Uribe <daniel.uribeporfirio@inl.gov>
2025-12-04 10:29:00 -0700

    removed console logs and added project session to local storage to easily get even after a refresh.

--- deeplynx.UI/src/app/lib/client_service/projects_services.client.ts
+++ deeplynx.UI/src/app/lib/client_service/projects_services.client.ts
@@ -160,7 +160,6 @@ export async function getProjectStats(
     const res = await api.get(
       `/organizations/${organizationId}/projects/${projectId}/stats`
     );
-    console.log("Project stats: ", res)
     return res.data;
   } catch (error) {
     console.error(`Error getting stats for project ${projectId}:`, error);
